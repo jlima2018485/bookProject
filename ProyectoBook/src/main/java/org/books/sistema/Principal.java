003c29867b995cdd2b8a40c00303cf00b8b2a8d8
Adolfo_Lopez <alopez-2018176@kinal.edu.gt>
2019-09-04 16:51:16 -0600

    Create case 4 and bucle for menu

--- ProyectoBook/src/main/java/org/books/sistema/Principal.java
+++ ProyectoBook/src/main/java/org/books/sistema/Principal.java
@@ -2,6 +2,7 @@ package org.books.sistema;
 
 import java.util.Scanner;
 import org.books.dao.LibroJpaController;
+import org.books.dao.exceptions.NonexistentEntityException;
 import org.books.dominio.Libro;
 
 public class Principal {
@@ -13,49 +14,67 @@ public class Principal {
     private static int op, id;
 
     public static void main(String[] args) {
+        do{
+            System.out.println("");
+            System.out.println("***********************************************************");
+            System.out.println("***********************************************************");
+            System.out.println("*******************[ MENU DE OPCIONES ]********************");
+            System.out.println("***********************************************************");
+            System.out.println("***********************************************************");
+            System.out.println("___________________________________________________________");
+            System.out.println(" ~1 Mostrar Persona Por Codigo                             ");
+            System.out.println(" ~2 Agregar Persona Al Sitema Gestor Base De Datos         ");
+            System.out.println(" ~3 Actualizar La Tupla                                    ");
+            System.out.println(" ~4 Eliminar La Tupla                                      ");
+            System.out.println(" ~5 Listar Persona                                         ");
+            System.out.println(" ~6 Salir                                                  ");
+            System.out.println("___________________________________________________________");
+            System.out.println("  Ingrese Una Opcion A Realizar Por Favor...               ");
+            System.out.println("___________________________________________________________");
+            System.out.println("");
 
-        System.out.println("");
-        System.out.println("***********************************************************");
-        System.out.println("***********************************************************");
-        System.out.println("*******************[ MENU DE OPCIONES ]********************");
-        System.out.println("***********************************************************");
-        System.out.println("***********************************************************");
-        System.out.println("___________________________________________________________");
-        System.out.println(" ~1 Mostrar Persona Por Codigo                             ");
-        System.out.println(" ~2 Agregar Persona Al Sitema Gestor Base De Datos         ");
-        System.out.println(" ~3 Actualizar La Tupla                                    ");
-        System.out.println(" ~4 Eliminar La Tupla                                      ");
-        System.out.println(" ~5 Listar Persona                                         ");
-        System.out.println(" ~6 Salir                                                  ");
-        System.out.println("___________________________________________________________");
-        System.out.println("  Ingrese Una Opcion A Realizar Por Favor...               ");
-        System.out.println("___________________________________________________________");
-        System.out.println("");
+            op = sc.nextInt();
 
-        op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    System.out.println("¿Desea regresar al menú Principal?");
+                    break;
+                case 2:
+                    System.out.println("¿Desea regresar al menú Principal?");
+                    break;
+                case 3:
+                    System.out.println("¿Desea regresar al menú Principal?");
+                    break;
+                case 4:
+                    b = obtenerLibro();
+                    System.out.println("¿Está seguro que desea eliminar el siguiente registro?");
+                    System.out.println(b);
 
-        switch (op) {
-            case 1:
+                    try {
+                        bookDao.destroyBook(id);
+                        System.out.println("Registro eliminado éxitosamente");
+                        System.out.println("¿Desea regresar al menú Principal?");
+                    } catch (NonexistentEntityException ex) {
+                        System.out.println("No fue posible eliminar el registro");
+                        System.out.println("¿Desea regresar al menú Principal?");
+                    }
+                    break;
+                case 5:
+                    System.out.println("¿Desea regresar al menú Principal?");
+                    break;
+                case 6:
+                    System.out.println("------------------ Cerrando el Programa ------------------");
+                    System.out.println("--------------- Que tenga un excelente día ---------------");
+                    bookDao.cerrar();
+                    break;
 
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-
-                break;
-
-            default:
-                System.out.println("No Es Una Opcion Del Menu");
-        }
+                default:
+                    System.out.println("No Es Una Opcion Del Menu");
+                    System.out.println("¿Desea intentarlo de nuevo?");
+            }
+        }while(sbook.nextLine().equalsIgnoreCase("si"));
+            System.out.println("------------------ Cerrando el Programa ------------------");
+            System.out.println("--------------- Que tenga un excelente día ---------------");
+            bookDao.cerrar();
     }
 }
