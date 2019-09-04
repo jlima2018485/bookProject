75d467f1e8983f7268b74eb3cc6654823e3e356f
mmazariegos-2015232 <jrivera-2018496@kinal.edu.gt>
2019-09-04 16:22:51 -0600

    create menu

--- ProyectoBook/src/main/java/org/books/sistema/Principal.java
+++ ProyectoBook/src/main/java/org/books/sistema/Principal.java
@@ -5,12 +5,57 @@ import org.books.dao.LibroJpaController;
 import org.books.dominio.Libro;
 
 public class Principal {
+
     private static Scanner sc = new Scanner(System.in);
-    private static Scanner sbook= new Scanner(System.in);
+    private static Scanner sbook = new Scanner(System.in);
     private static Libro b;
-    private static LibroJpaController bookDao= new LibroJpaController();
+    private static LibroJpaController bookDao = new LibroJpaController();
     private static int op, id;
+
     public static void main(String[] args) {
-        
+
+        System.out.println("");
+        System.out.println("***********************************************************");
+        System.out.println("***********************************************************");
+        System.out.println("*******************[ MENU DE OPCIONES ]********************");
+        System.out.println("***********************************************************");
+        System.out.println("***********************************************************");
+        System.out.println("___________________________________________________________");
+        System.out.println(" ~1 Mostrar Persona Por Codigo                             ");
+        System.out.println(" ~2 Agregar Persona Al Sitema Gestor Base De Datos         ");
+        System.out.println(" ~3 Actualizar La Tupla                                    ");
+        System.out.println(" ~4 Eliminar La Tupla                                      ");
+        System.out.println(" ~5 Listar Persona                                         ");
+        System.out.println(" ~6 Salir                                                  ");
+        System.out.println("___________________________________________________________");
+        System.out.println("  Ingrese Una Opcion A Realizar Por Favor...               ");
+        System.out.println("___________________________________________________________");
+        System.out.println("");
+
+        op = sc.nextInt();
+
+        switch (op) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+
+            default:
+                System.out.println("No Es Una Opcion Del Menu");
+        }
     }
 }
