bfed804d595c5fe6dc5ff3649be269dfc2571fdf
Jalavarado2018366 <programacion@fkinal.edu>
2019-09-04 16:04:34 -0600

    create Dao

---
+++ ProyectoBook/src/main/java/org/books/dao/LibroJpaController.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.books.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.books.dao.exceptions.NonexistentEntityException;
+import org.books.dao.exceptions.PreexistingEntityException;
+import org.books.dominio.Libro;
+
+/**
+ *
+ * @author programacion
+ */
+public class LibroJpaController implements Serializable {
+    private EntityManagerFactory emf = null;
+
+    public LibroJpaController() {
+        this.emf = Persistence.createEntityManagerFactory("DB_BOOKS");
+    }
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public boolean addBook(Libro libro){
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(libro);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            return false;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return true;
+    }
+
+    public void editBook(Libro libro) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            libro = em.merge(libro);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                int id = libro.getCodigoLibro();
+                if (findLibro(id) == null) {
+                    throw new NonexistentEntityException("The libro with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroyBook(int id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Libro libro;
+            try {
+                libro = em.getReference(Libro.class, id);
+                libro.getCodigoLibro();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The libro with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(libro);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Libro> findLibroEntities() {
+        return findLibroEntities(true, -1, -1);
+    }
+
+    public List<Libro> findLibroEntities(int maxResults, int firstResult) {
+        return findLibroEntities(false, maxResults, firstResult);
+    }
+
+    private List<Libro> findLibroEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Libro.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Libro findLibro(int id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Libro.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getLibroCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Libro> rt = cq.from(Libro.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    public void cerrar(){
+        emf.close();    
+    }   
+    
+}
--- ProyectoBook/src/main/java/org/books/sistema/Principal.java
+++ ProyectoBook/src/main/java/org/books/sistema/Principal.java
@@ -1,6 +1,15 @@
 package org.books.sistema;
 
+import java.util.Scanner;
+import org.books.dao.LibroJpaController;
+import org.books.dominio.Libro;
+
 public class Principal {
+    private static Scanner sc = new Scanner(System.in);
+    private static Scanner sbook= new Scanner(System.in);
+    private static Libro b;
+    private static LibroJpaController bookDao= new LibroJpaController();
+    private static int op, id;
     public static void main(String[] args) {
         
     }
